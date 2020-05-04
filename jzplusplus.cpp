350d2d97c637f9c3a8228c26939f4ce008ac8073
JohnZelman <jrzelman@gmail.com>
2020-05-03 20:40:48 -0500

    What I've done today.
    An update to everything based on today's work.

---
+++ jzplusplus.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Hello, world!
+    cout << "Hello, world!" << endl;
+
+    //Example for making a shape in-line with printing in C++
+    cout << "       /|" << endl;
+    cout << "      / |" << endl;
+    cout << "     /  |" << endl;
+    cout << "    /   |" << endl;
+    cout << "   /    |" << endl;
+    cout << "  /     |" << endl;
+    cout << " /      |" << endl;
+    cout << "/_______|" << endl;
+
+    //Testing out variables.
+    string name, description;
+
+    cout <<"Enter your name: " << endl;
+    getline (cin, name);
+    cout <<"What do you think of this triangle? " << endl;
+    getline (cin, description);
+
+    cout << "This triangle was made by " << name << " and it is " << description << ", isn't that neat? " << endl;
+
+    return 0;
+}
